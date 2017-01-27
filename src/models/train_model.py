e07904d965385e1c2512d9c74c4e6516bfa0aadc
Swetha Subramanian <swethasubramanian@gmail.com>
2017-01-27 13:58:00 -0800

    added CNN model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,35 +1,86 @@
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
 
-import tensorflow as tf
 import pickle
+import pandas as pd 
+import numpy as np 
 
-from datafeed import Dataset
 
-import pandas as pd
+# Load HDF5 dataset
+import h5py
+h5f = h5py.File('../data/traindataset.h5', 'r')
+X_train_images = h5f['X']
+Y_train_labels = h5f['Y']
 
-class LungModel(object):
-	'''
-	Will initialize model here
-	'''
+h5f2 = h5py.File('../data/valdataset.h5', 'r')
+X_val_images = h5f2['X']
+Y_val_labels = h5f2['Y']
 
-	def __init__(self):
-		
+h5f2 = h5py.File('../src/data/testdataset.h5', 'r')
+X_test_images = h5f2['X']
+Y_test_labels = h5f2['Y']
 
-## Feed trining data into tensorflow in batches
-X_train = pd.read_pickle('traindata')
-y_train = pd.read_pickle('trainlabels')
+# Make sure the data is normalized
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
 
-# Start with the augmented trained dataset
-tempDf = X_train[y_train == 1]
-tempDf = tempDf.set_index(X_train[y_train == 1].index + 1000000)
-X_train_new = X_train.append(tempDf)
-tempDf = tempDf.set_index(X_train[y_train == 1].index + 2000000)
-X_train_new = X_train_new.append(tempDf)
+# Create extra synthetic training data by flipping, rotating and blurring the
+# images on our data set.
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+img_aug.add_random_blur(sigma_max=3.)
 
-ytemp = y_train.reindex(X_train[y_train == 1].index + 1000000)
-ytemp.loc[:] = 1
-y_train_new = y_train.append(ytemp)
-ytemp = y_train.reindex(X_train[y_train == 1].index + 2000000)
-ytemp.loc[:] = 1
-y_train_new = y_train_new.append(ytemp)
+# Input is a 50x50 image with 1 color channels (grayscale)
+network = input_data(shape=[None, 50, 50, 1],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+
+# Step 1: Convolution
+network = conv_2d(network, 50, 3, activation='relu')
+
+# Step 2: Max pooling
+network = max_pool_2d(network, 2)
+
+# Step 3: Convolution again
+network = conv_2d(network, 64, 3, activation='relu')
+
+# Step 4: Convolution yet again
+network = conv_2d(network, 64, 3, activation='relu')
+
+# Step 5: Max pooling again
+network = max_pool_2d(network, 2)
+
+# Step 6: Fully-connected 512 node neural network
+network = fully_connected(network, 512, activation='relu')
+
+# Step 7: Dropout - throw away some data randomly during training to prevent over-fitting
+network = dropout(network, 0.5)
+
+# Step 8: Fully-connected neural network with two outputs (0=isn't a nodule, 1=is a nodule) to make the final prediction
+network = fully_connected(network, 2, activation='softmax')
+
+# Tell tflearn how we want to train the network
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Wrap the network in a model object
+model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='nodule-classifier.tfl.ckpt')
+
+# Train it! We'll do 100 training passes and monitor it as it goes.
+model.fit(X_train_images, Y_train_labels, n_epoch=100, shuffle=True, validation_set=(X_val_images, Y_val_labels),
+          show_metric=True, batch_size=96,
+          snapshot_epoch=True,
+          run_id='nodule-classifier')
+
+# Save model when training is complete to a file
+model.save("nodule-classifier.tfl")
+print("Network trained and saved as nodule-classifier.tfl!")
+
 
 
