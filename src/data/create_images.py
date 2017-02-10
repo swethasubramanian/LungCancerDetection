d464d070e5f810a101e8ff45d14febcd3a93525f
Swetha Subramanian <swethasubramanian@gmail.com>
2017-02-10 15:50:43 -0800

    modified create_images.py

--- src/data/create_images.py
+++ src/data/create_images.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+from joblib import Parallel, delayed
+import multiprocessing
+
+import pickle
+
 import numpy as np
 import pandas as pd
 
@@ -110,5 +119,40 @@ class CTScan(object):
         image = self.get_subimage(width)
         image = self.normalizePlanes(image)
         Image.fromarray(image*255).convert('L').save(filename)
+
+
+def create_data(idx, outDir, width = 50):
+    '''
+    Generates your test, train, validation images
+    outDir = a string representing destination
+    width (int) specify image size
+    '''
+    scan = CTScan(np.asarray(X_data.loc[idx])[0], \
+        np.asarray(X_data.loc[idx])[1:], raw_image_path)
+    outfile = outDir  +  str(idx)+ '.jpg'
+    scan.save_image(outfile, width)
+
+
+def main():
+    if len(sys.argv) < 2:
+        raise ValueError('1 argument needed. Specify if you need to generate a train, test or val set')
+    else:
+        mode = sys.argv[1]
+        if mode not in ['train', 'test', 'val']:
+            raise ValueError('Argument not recognized. Has to be train, test or val')
+
+    inpfile = mode + 'data'
+    outDir = mode + '/image_'
+    X_data = pd.read_pickle(inpfile)
+    raw_image_path = '../../data/raw/*/'
+
+    # Parallelizes inorder to generate more than one image at a time
+    Parallel(n_jobs = 3)(delayed(create_data)(idx, outDir) for idx in X_data.index)
+
+if __name__ == "__main__":
+    main()
+
+
+
 
         
